c98b28907a258907d0df7d5f89a6530196db7186
Ansticefish <ansticefish0106@gmail.com>
2022-04-27 15:39:23 +0800

    Fix style to meet JS standard style

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        additionalData: `@import "~@/assets/scss/variables&mixins.scss";`
-      },
+        additionalData: '@import "~@/assets/scss/variables&mixins.scss";'
+      }
     }
   }
-};
+}
